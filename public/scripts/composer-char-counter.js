9c458d771c4e8b58ddaddd420e12d06823b59b90
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-10-08 17:32:34 +0000

    Add functionality to word counter for tweets, cleanup CSS code

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+// Function for Tweet Word counter
+
+$(document).ready(function() {
+  $("#tweet-box").keyup(function() {
+    let length = $(this).val().length;
+    let counter = 140;
+    if (counter - length < 0) {
+      $(".counter").css("color", "red");
+    } else{
+      $(".counter").css("color", "rgb(75, 49, 146)");
+    }
+    $(".counter").text(counter - length);
+  });
+});
